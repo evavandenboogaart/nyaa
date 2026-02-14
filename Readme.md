32efe0ff903e6a65ad8d5c4e542c2a9be1631c6f
eva <diarmidodina@gmail.com>
2026-02-14 15:13:23 +0100

    nyaarn

---
+++ Readme.md
@@ -0,0 +1,4 @@
+## Usage
+
+Go to nyaa, set your filters, make sure the team is first, automatically tries in alphabetical order by adding a space behind the first search term and then the alphabetic character, make sure to end the url with &page=1, wrap the url in quotation
+`node . "{url}"`
---
+++ index.js
@@ -0,0 +1,198 @@
+import fs from "fs";
+import { parse } from "node-html-parser";
+import { Readable } from "stream";
+
+const downloadThreads = 4;
+const nyaa_max_results = 1000;
+const max_retries = 4;
+
+const alphabet = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+];
+
+(async () => {
+  var args = process.argv.slice(2)[0];
+  let downloadLinks = [];
+
+  const appendLinks = async (href, letter, isSameLetter) => {
+    const url = new URL(href);
+    if (!isSameLetter && letter) {
+      url.searchParams.set(
+        "q",
+        `"${url.searchParams.get("q").split(" ")[0]}+${letter}"+${url.searchParams.get("q").split(" ")[1]}`,
+      );
+
+      url.search = decodeURIComponent(url.search);
+    } else if (!isSameLetter) {
+      url.searchParams.set(
+        "q",
+        `${url.searchParams.get("q").split(" ")[0]} +-${alphabet.map((localLetter) => `"${url.searchParams.get("q").split(" ")[0]}+${localLetter}"`).join("|")}+${url.searchParams.get("q").split(" ")[1]}`,
+      );
+
+      url.search = decodeURIComponent(url.search);
+    }
+
+    try {
+      console.clear();
+      console.log(
+        `(${downloadLinks.length}) Retrieving links in alphanumerical order (${letter})`,
+      );
+      const { body: localBody } = await fetch(url);
+
+      const localPlainTextBody = await new Response(localBody).text();
+
+      const localDocument = parse(localPlainTextBody);
+
+      const tooManyRequests = localPlainTextBody.includes(
+        "429 Too Many Requests",
+      );
+      if (tooManyRequests) {
+        console.log("429: Too many requests");
+        Promise.resolve();
+        return;
+      }
+
+      const noTorrents = localPlainTextBody.includes(" 0 results");
+      if (noTorrents) {
+        Promise.resolve();
+        return;
+      }
+      const maxResults = localPlainTextBody.includes(
+        `${nyaa_max_results} results`,
+      );
+      if (maxResults) {
+        for (let j = 0; j < alphabet.length; j++) {
+          await appendLinks(href, letter + alphabet[j], isSameLetter);
+        }
+        Promise.resolve();
+        return;
+      }
+      const localLinks = localDocument.getElementsByTagName("a");
+
+      [].slice.call(localLinks).reduce(async (acc, anchor) => {
+        const href = anchor.getAttribute("href");
+        if (href && href.endsWith(".torrent")) {
+          downloadLinks.push(href);
+        }
+        return acc;
+      }, []);
+
+      const lists = localDocument.getElementsByTagName("ul");
+      const lastList = lists[lists.length - 1];
+      const listItems = lastList?.getElementsByTagName("li");
+      const activePage = Number(url.searchParams.get("page"));
+      const nextListItem = listItems?.find(
+        ({ childNodes }) =>
+          !Number.isNaN(Number(childNodes[0].innerHTML)) &&
+          Number(childNodes[0].innerHTML) > activePage,
+      );
+
+      if (nextListItem) {
+        url.searchParams.set("page", Number(url.searchParams.get("page")) + 1);
+        url.search = decodeURIComponent(url.search);
+        await appendLinks(url, letter, true);
+      }
+    } catch (error) {
+      console.log(error?.message);
+      await appendLinks(href, letter, isSameLetter);
+    }
+  };
+
+  for (let i = 0; i < alphabet.length; i++) {
+    await appendLinks(args, alphabet[i]);
+  }
+  await appendLinks(args, false);
+
+  const downloadFile = async (index, retryCount) => {
+    const split = downloadLinks[index].split("/");
+    const fileName = split[split.length - 1];
+
+    split.pop();
+    const path = split.join("/");
+
+    await fs.promises.mkdir(
+      `./dump/${decodeURI(path)}`,
+      { recursive: true },
+      (err) => {
+        console.log(err);
+      },
+    );
+
+    const downloadUrl = `${downloadLinks[index].startsWith("/") ? args.replace(/\/\?.*/gim, "") : ""}${downloadLinks[index]}`;
+    console.clear();
+    console.log(`downloading file ${index + 1} of ${downloadLinks.length}`);
+    const filePath = `./dump/${decodeURI(path)}/${decodeURI(fileName)}`;
+    await fetch(downloadUrl)
+      .then((response) =>
+        Readable.fromWeb(response.body).pipe(
+          fs.unlink(filePath, () =>
+            fs.createWriteStream(filePath, { flags: "wx" }).catch((err) => {
+              console.log(err);
+              if (index + (downloadThreads || 1) < downloadLinks.length) {
+                downloadFile(index + (downloadThreads || 1));
+              }
+            }),
+          ),
+        ),
+      )
+      .catch((err) => {
+        if (retryCount === max_retries) {
+          console.log("Max retries attempted");
+        } else {
+          console.log("Fetch failed, retrying");
+          console.log(err);
+          downloadFile(index, retryCount + 1);
+        }
+      });
+    if (index + (downloadThreads || 1) < downloadLinks.length) {
+      downloadFile(index + (downloadThreads || 1));
+    }
+  };
+
+  downloadLinks = downloadLinks.reduce((acc, downloadLink) => {
+    if (!acc.includes(downloadLink)) acc.push(downloadLink);
+    return acc;
+  }, []);
+  if (downloadThreads) {
+    Array.from({ length: downloadThreads }).forEach((_, index) => {
+      downloadFile(index);
+    });
+  } else {
+    downloadFile(0);
+  }
+})();
